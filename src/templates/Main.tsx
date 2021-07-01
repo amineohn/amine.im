b88cf446584ea1fc26ed8c31c5c2da0ed30fe9ba
Amine <imveny>
2021-07-01 23:13:28 +0200

    fix: issue with mobile and remove useless css rules

--- src/templates/Main.tsx
+++ src/templates/Main.tsx
@@ -16,7 +16,7 @@ const Main = (props: IMainProps) => {
     <>
       {props.meta}
 
-      <div className="sticky top-0 z-50 flex items-center justify-between w-full max-w-4xl p-8 mx-auto my-0 text-gray-900 bg-white dark:bg-black bg-opacity-60 dark:bg-opacity-60 backdrop-filter backdrop-brightness-50 backdrop-blur-sm md:my-8 dark:text-gray-100">
+      <div className="sticky top-0 z-50 flex items-center justify-between w-full max-w-4xl p-8 mx-auto my-0 text-gray-900 bg-white dark:bg-black md:my-8 dark:text-gray-100">
         <ThemeSwitcher />
         <div>
           <a
