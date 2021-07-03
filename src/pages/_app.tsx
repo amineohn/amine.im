b148534b31025eebedbe601ac80e72dc2dd5f65e
Amine <imveny>
2021-07-03 12:02:16 +0200

    style: make default theme system

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,9 @@
-import { ThemeProvider } from 'next-themes';
-import { AppProps } from 'next/app';
-import '../styles/main.css';
+import { ThemeProvider } from "next-themes";
+import { AppProps } from "next/app";
+import "../styles/main.css";
 
 const App = ({ Component, pageProps }: AppProps) => (
-  <ThemeProvider defaultTheme="light" attribute="class">
+  <ThemeProvider defaultTheme="system" attribute="class">
     <Component {...pageProps} />
   </ThemeProvider>
 );
