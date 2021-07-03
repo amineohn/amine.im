b9a4d6aa8d2876e087c877d22ac7eb9a43a02416
Amine <imveny>
2021-07-03 11:28:20 +0200

    refactor: create constants file to manage all constants in this file.

---
+++ src/constants/constants.ts
@@ -0,0 +1,38 @@
+export const Projects = [
+  {
+    name: "title project",
+    link: "http://",
+    image: "https://images.unsplash.com/photo-1579546929518-9e396f3cc809",
+    resume: "resume project",
+  },
+  {
+    name: "title project",
+    link: "http://",
+    image: "https://images.unsplash.com/photo-1579546929518-9e396f3cc809",
+    resume: "resume project",
+  },
+  {
+    name: "title project",
+    link: "http://",
+    image: "https://images.unsplash.com/photo-1579546929518-9e396f3cc809",
+    resume: "resume project",
+  },
+  {
+    name: "title project",
+    link: "http://",
+    image: "https://images.unsplash.com/photo-1579546929518-9e396f3cc809",
+    resume: "resume project",
+  },
+  {
+    name: "title project",
+    link: "http://",
+    image: "https://images.unsplash.com/photo-1579546929518-9e396f3cc809",
+    resume: "resume project",
+  },
+  {
+    name: "title project",
+    link: "http://",
+    image: "https://images.unsplash.com/photo-1579546929518-9e396f3cc809",
+    resume: "resume project",
+  },
+];
--- src/pages/project.tsx
+++ src/pages/project.tsx
@@ -1,43 +1,6 @@
 import { Meta } from "../layout/Meta";
 import { Main } from "../templates/Main";
-const Projects = [
-  {
-    name: "title project",
-    link: "http://",
-    image: "https://images.unsplash.com/photo-1579546929518-9e396f3cc809",
-    resume: "resume project",
-  },
-  {
-    name: "title project",
-    link: "http://",
-    image: "https://images.unsplash.com/photo-1579546929518-9e396f3cc809",
-    resume: "resume project",
-  },
-  {
-    name: "title project",
-    link: "http://",
-    image: "https://images.unsplash.com/photo-1579546929518-9e396f3cc809",
-    resume: "resume project",
-  },
-  {
-    name: "title project",
-    link: "http://",
-    image: "https://images.unsplash.com/photo-1579546929518-9e396f3cc809",
-    resume: "resume project",
-  },
-  {
-    name: "title project",
-    link: "http://",
-    image: "https://images.unsplash.com/photo-1579546929518-9e396f3cc809",
-    resume: "resume project",
-  },
-  {
-    name: "title project",
-    link: "http://",
-    image: "https://images.unsplash.com/photo-1579546929518-9e396f3cc809",
-    resume: "resume project",
-  },
-];
+import { Projects } from "../constants/constants";
 const About = () => {
   return (
     <Main meta={<Meta title="Projects" description="My Projects" />}>
