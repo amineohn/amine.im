dc13f3a618cde2f478842bed1937f109ac55d844
Amine <imveny>
2021-07-03 23:25:45 +0200

    transition

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,7 +18,7 @@ const Index = () => {
   const { repos }: any = Repos;
   if (!repos || repos.length === 0)
     return (
-      <div className="fixed top-0 bottom-0 left-0 right-0 z-50 flex flex-col items-center justify-center w-full h-screen overflow-hidden bg-white opacity-75 dark:bg-black">
+      <div className="fixed top-0 bottom-0 left-0 right-0 z-50 flex flex-col items-center justify-center w-full h-screen overflow-hidden transition-all bg-white opacity-75 dark:bg-black">
         <div className="w-12 h-12 mb-4 ease-linear border-4 border-t-4 border-gray-100 rounded-full loader"></div>
       </div>
     );
