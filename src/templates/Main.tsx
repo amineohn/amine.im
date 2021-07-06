8c04ea68c2f14092e2f6bbd4fa9cd4ff73ab9f57
Amine <imveny>
2021-07-06 21:55:23 +0200

    test: weebhook

--- src/templates/Main.tsx
+++ src/templates/Main.tsx
@@ -16,7 +16,6 @@ const Main = (props: IMainProps) => {
   return (
     <>
       {props.meta}
-
       <div className="sticky top-0 z-50 flex items-center justify-between w-full max-w-4xl p-8 mx-auto my-0 text-gray-900 bg-white dark:bg-black md:my-8 dark:text-gray-100">
         <ThemeSwitcher />
         <div>
