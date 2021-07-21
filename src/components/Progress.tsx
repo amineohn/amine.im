31b609819d550ea844ec898785424445fba73b35
Amine <imveny>
2021-07-21 18:02:55 +0200

    style: colors

--- src/components/Progress.tsx
+++ src/components/Progress.tsx
@@ -3,9 +3,9 @@ import styled from "styled-components";
 
 const Progress = ({ percentage }: { percentage: number }) => {
   return (
-    <Container className="border border-blue-300 dark:border-yellow-50">
+    <Container className="border border-nightBlue dark:border-red-500">
       <ProgressFill
-        className="bg-gradient-to-l from-sky-400 to-blue-500 dark:bg-gradient-to-r dark:from-red-800 dark:via-yellow-600 dark:to-yellow-500"
+        className="bg-gradient-to-r from-nightBlue to-turquoise dark:bg-gradient-to-r dark:from-red-500 dark:to-red-900"
         initial={false}
         transition={{ ease: "easeOut", duration: 2 }}
         animate={{ x: `${percentage - 100}%` }}
