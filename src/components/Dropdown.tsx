f85542b25ac909d078acccef4ed74c3055cb1764
Amine <imveny>
2021-07-26 21:45:14 +0200

    damn it.

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -56,7 +56,7 @@ export const Dropdown = () => {
                 exit={{ opacity: 0, scale: 0.5 }}
                 transition={{ duration: 0.2 }}
                 onMouseLeave={() => setActive(false)}
-                className="absolute grid flex-col items-end gap-3 py-3 pl-4 pr-12 ml-5 text-xs font-normal bg-white border rounded border-turquoise dark:border-indigo-400 dark:bg-gray-800"
+                className="absolute grid flex-col items-end gap-3 py-3 pl-4 pr-12 ml-5 text-xs font-normal bg-white border rounded border-turquoise dark:border-indigo-400 dark:bg-black"
               >
                 <NextLink href="#">
                   <a
